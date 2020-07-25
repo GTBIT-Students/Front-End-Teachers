dc633cf2b3084d89e302cdaf358854b5e82b5a24
Sachin negi <san.negi.77777@gmail.com>
2020-07-25 12:51:42 +0530

    build folder added

---
+++ build/precache-manifest.4ca4ea284a5c7cf0d221f01e76267c15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3892b3b392ea4b07c90b3ac331163351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56bd056f7e2f6834ea5d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "608033b9afbf6a73cd30",
+    "url": "/static/css/main.413792e0.chunk.css"
+  },
+  {
+    "revision": "56bd056f7e2f6834ea5d",
+    "url": "/static/js/2.7b46a146.chunk.js"
+  },
+  {
+    "revision": "d7b319d8a6f4d5773bf6cc977c7731d1",
+    "url": "/static/js/2.7b46a146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608033b9afbf6a73cd30",
+    "url": "/static/js/main.d94daedd.chunk.js"
+  },
+  {
+    "revision": "907ea6149720ae1c9910",
+    "url": "/static/js/runtime-main.f3670a13.js"
+  }
+]);
